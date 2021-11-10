b687313337921a8a65609769d9b3056aecad85e1
AliAhsan <68770090+aliahsan786@users.noreply.github.com>
2021-11-10 11:55:31 +0500

    CGPA

---
+++ CGPA/lib/screens/HomeScreen.dart
@@ -0,0 +1,78 @@
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'input_page.dart';
+import 'results_page.dart';
+
+class HomeScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[ Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  onPress: () { Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => InputPage(
+
+                        ),
+                      )
+                  );
+
+                  },
+
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.pager,
+                    label: 'GPA',
+                  ),
+                ),
+              ),Expanded(
+                child: ReusableCard(
+                  onPress: () { Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => InputPage(
+
+                        ),
+                      )
+                  );
+                  },
+
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.pager,
+                    label: 'SGPA',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  onPress: () {Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => InputPage(
+
+                      ),
+                    )
+                  );
+
+                  },
+
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.pager,
+                    label: 'CGPA',
+                  ),
+                ),
+              ),
+            ],
+          )),
+
+ ]));}}
